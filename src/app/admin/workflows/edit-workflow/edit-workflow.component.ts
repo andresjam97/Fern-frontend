bdc6b7d911f5ecea1faf7aff5959de192fca255f
Daniel Burgos <danielfburgos9806@gmail.com>
2024-01-07 20:42:45 -0500

    Add basic edit of workflow view

--- src/app/admin/workflows/edit-workflow/edit-workflow.component.ts
+++ src/app/admin/workflows/edit-workflow/edit-workflow.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { WorkflowService } from '../workflow.service';
+import { Workflow } from '../workflow';
 
 @Component({
   selector: 'app-edit-workflow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './edit-workflow.component.html',
   styleUrls: ['./edit-workflow.component.css']
 })
-export class EditWorkflowComponent {
+export class EditWorkflowComponent implements OnInit {
+  workflow: Workflow | undefined;
 
+  constructor(private route: ActivatedRoute, private service: WorkflowService) { }
+
+  ngOnInit(): void {
+    const paramsMap = this.route.snapshot.paramMap;
+    const workflowId = paramsMap.get('workflowId');
+
+    if (!workflowId) return;
+
+    this.service.getWorkflowById(workflowId).subscribe((workflow: Workflow) => this.workflow = workflow);
+  }
 }
--- src/app/admin/workflows/workflow.ts
+++ src/app/admin/workflows/workflow.ts
@@ -1,5 +1,9 @@
+import { Stage } from "./stage";
+
 export interface Workflow {
     id: string;
     title: string;
     status: string;
+    description: string;
+    stages: Stage[];
 }
